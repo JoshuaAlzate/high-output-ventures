7c6dea0e1a99f9a58d0c7141fa893e177fb323ba
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-01-31 10:21:16 +0800

    Added Search Component

---
+++ components/search.tsx
@@ -0,0 +1,39 @@
+import { SearchIcon } from "@chakra-ui/icons";
+import { Box, Button, Container, Flex, Select } from "@chakra-ui/react";
+import { useState } from "react";
+import { Category } from "../model/category.model";
+
+const Search = ({ categories, locations } : { categories: Category[], locations: { code: string, name: string }}) => {
+    const [category, setCategory] = useState('');
+    const [location, setLocation] = useState('');
+
+    const searchAction = async () => {
+    }
+
+    return (
+        <Container maxW='container.lg' centerContent display={'block'}>
+            <Flex direction={['column', 'row']}>
+                <Box flex={[1, 6]}>
+                    What
+                    <Select value={category} placeholder='Select Category' onChange={event => setCategory(event.target.value)}>
+                        {categories ? categories.map(category => <option value={category.alias} key={category.alias}>{category.title}</option>) : ''}
+                    </Select>
+                </Box>
+
+                <Box flex={[1, 4]}>
+                    Where
+                    <Select value={location} placeholder='Select Location' onChange={event => setLocation(event.target.value)}>
+                        {locations ? locations.map(location => <option value={location.code} key={location.code}>{location.name}</option>) : ''}
+                    </Select>
+                </Box>
+                <Button flex={[1, 2]} py={['10px', 'unset']} alignSelf={['normal', 'flex-end']} onClick={searchAction}>
+                    <SearchIcon mr={2} />
+                    Search
+                </Button>
+
+            </Flex>
+        </Container>
+    )
+}
+
+export default Search;
