ec70d9ae043f40165b3fe46263d2f2aa905f1e72
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-01-31 10:22:31 +0800

    Added Search Components to Index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,25 @@
 import { Container, Stack, Text } from '@chakra-ui/react'
-import type { NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next'
+import Search from '../components/search'
+import SearchResult from '../components/search-result'
+import api_categories from '../categories.json';
+import api_location from '../countries.json';
+import { Category } from '../model/category.model';
+import { useSelector } from 'react-redux';
 
-const Home: NextPage = () => {
+
+
+export const getStaticProps: GetStaticProps<{ categories: Category[], locations: any }> = async () => {
+  return {
+    props: {
+      categories: api_categories,
+      locations: api_location
+    },
+  };
+}
+
+const Home = ({ categories, locations }) => {
+  const businessSearchResult = useSelector(state => state['businessSearchResult']);
   return (
     <>
       <Container py={10}>
@@ -10,7 +28,10 @@ const Home: NextPage = () => {
           <Text fontSize='4xl'>Your astonishing city!</Text>
         </Stack>
       </Container>
+      <Search categories={categories} locations={locations} />
+      <SearchResult businesses={businessSearchResult} />
     </>
+
   )
 }
 
